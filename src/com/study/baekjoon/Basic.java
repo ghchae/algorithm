3edf4601fe9a1a858a641ec746bb1a4b948c360f
ghchae <cvxgihyun@gmail.com>
2022-07-19 00:02:44 +0900

    StringTokenizer , String.split() 비교

--- src/com/study/baekjoon/Basic.java
+++ src/com/study/baekjoon/Basic.java
@@ -6,7 +6,8 @@ import java.util.StringTokenizer;
 
 public class Basic {
     public static void main(String[] args) throws IOException {
-        빠른A더하기B();
+//        빠른A더하기B();
+        빠른더하기_split();
     }
 
     public static void 빠른A더하기B() throws IOException {
@@ -22,6 +23,22 @@ public class Basic {
             }
             bw.write(String.valueOf(sum) + "\n");
             sum = 0;
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static void 빠른더하기_split() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(br.readLine());
+        for (int i = 0; i < count; i++) {
+            String str = br.readLine();
+            String[] arr = str.split(" ");
+            int A = Integer.parseInt(arr[0]);
+            int B = Integer.parseInt(arr[1]);
+            bw.write(A + B + "\n");
         }
         bw.flush();
         bw.close();
